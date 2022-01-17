702c07cd13cec871b0c05723d79b88264c9bd57a
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-01-17 17:47:14 -0300

    A1

--- Curso.js/AULA 1/app.js
+++ Curso.js/AULA 1/app.js
@@ -1,1 +1,5 @@
-console.log('Hello world!');
+console.log('"Filipe Sena" '); // String= Texto
+console.log("Filipe Sena ");
+console.log(`"Filipe" 'Sena'  `); 
+console.log(123456);
+console.log(15, 45, 46,47,67, 87, "Filipe");
---
+++ Curso.js/AULA 1/index.js
@@ -0,0 +1,7 @@
+/* 
+*bloco de código
+*Linha de comentário*/
+ console.log("Filipe") 
+ console.log("Luan")
+ console.log("Olá Mundo!!");
+ console.log("Filipe");
