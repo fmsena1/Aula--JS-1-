5d79ee2b2ff634a08948b3c98857028f1ec39054
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-01-18 17:42:32 -0300

    Update index.js

--- Curso.js/AULA 1/index.js
+++ Curso.js/AULA 1/index.js
@@ -1,7 +1,15 @@
-/* 
-*bloco de código
-*Linha de comentário*/
- console.log("Filipe") 
- console.log("Luan")
- console.log("Olá Mundo!!");
- console.log("Filipe");
+/* Regras */ 
+/*
+Não podemos criar constantes com paalvras reservadas
+Não podemos começar um nome de uma constante com um número
+usam camelCase
+NÃO PODE MODIFICAR O VALOR DE UMA CONSTANTE
+let e const não podem estar na declaraçã o entre aspas*/
+let primeironumero= 5;
+const segundonumero=10;
+const resultado= primeironumero * segundonumero;
+const resultadoDuplicado= resultado * 2 ;
+console.log (resultado);
+let resultadoTriplicado = resultado + 5 ;
+console.log (resultadoDuplicado);
+console.log (resultadoTriplicado + resultado);
