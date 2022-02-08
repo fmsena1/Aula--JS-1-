4204894ae51afb758aeddd5fe497fe2e469f0e02
Filipe Magalhães Sena <filipemagalhaessena@gmail.com>
2022-02-08 10:16:55 -0300

    Aulas 4-6

--- script.js
+++
@@ -1,7 +0,0 @@
-//arrays 
-
-let familia =[ true,26,'filipe',50,17];
-console.log (familia.length);
-console.log (familia);
-let nomeDoColega = ['Filipe',21, 'VCA']
-console.log (nomeDoColega);
--- scriptfunção.js
+++
@@ -1,8 +0,0 @@
-// Verbo ++SUBSTANTIVO
-let corSite= "azul";
-function resetaCor(cor,tonalidade){
-    corSite = cor + ' ' + tonalidade;
-};
-console.log(corSite);
-resetaCor("verde","claro");
-console.log(corSite);
